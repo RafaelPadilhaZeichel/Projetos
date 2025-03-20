170293a3c7439c77e69e48de7096a5bcf09c5efb
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-03-20 16:55:54 -0300

    Projeto de condições do tempo finalziado

---
+++ Condicoes-do-Tempo/Ts/script.ts
@@ -0,0 +1,39 @@
+const form = document.querySelector("#search-form > form");
+const input: HTMLInputElement | null =
+  document.querySelector("#input-localizacao");
+const sectionTempoInfo = document.querySelector("#tempo-info");
+
+form?.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  if (!input || !sectionTempoInfo) return;
+
+  const localizacao = input.value;
+
+  if (localizacao.length < 3) {
+    alert("O local precisa ter pelo menos três letras...");
+    return;
+  }
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=57ea381e6fc6ad06395da64c4fbabcce&lang=pt_br&units=metric`
+  );
+
+  const dados = await response.json();
+    
+  const infos = {
+    temperatura: Math.round(dados.main.temp),
+    local: dados.name,
+    icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+  };
+
+  sectionTempoInfo.innerHTML = `
+    <div class="tempo-dados">
+        <h2>${infos.local}</h2>
+
+        <span>${infos.temperatura}°C</span>
+    </div>
+
+      <img src="${infos.icone}">
+        `;
+});
